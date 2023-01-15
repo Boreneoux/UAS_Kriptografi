5fdf4ba1628a3fff1d28c46c5de70047338a3778
Boreneoux <ichlasul.ap@gmail.com>
2023-01-15 11:22:30 +0700

    add menu

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 print("===============")
 print("UAS KRIPTOGRAFI")
 print("===============")
+
+# MENU UTAMA PROGRAM
+
+while True:  
+    print("\nMAIN MENU")  
+    print("1. Asimetri Kriptografi untuk Citra/Gambar")  
+    print("2. Asimetri Kriptografi untuk Teks")  
+    print("3. Keluar Dari Program")  
+    choice1 = int(input("\n Masukkan Pilihan Menu Anda:"))  
+  
+    if choice1 == 1:  
+       print("\nPilihan 1")
+      
+    elif choice1 == 2:  
+        print("\nPilihan 2")  
+      
+    elif choice1 == 3:  
+        break  
+      
+    else:  
+        print("\n Oops! Menu yang dipilih ga ada nih!")  
